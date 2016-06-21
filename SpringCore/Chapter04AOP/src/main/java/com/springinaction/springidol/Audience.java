3f17169bc609a87ef541746976f45b30e4d32d14
liudebin <liudebin@99114.com>
2016-06-21 12:42:06 +0800

    Initial commit

---
+++ SpringCore/Chapter04AOP/src/main/java/com/springinaction/springidol/Audience.java
@@ -0,0 +1,60 @@
+package com.springinaction.springidol;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+/**
+ * 观众
+ * Created by dancer on 2016/6/19.
+ */
+public class Audience {
+
+    public void watchPerformance(ProceedingJoinPoint joinPoint){
+        System.out.println("The audience is taking their seats.");
+        System.out.println("The audience is turning off their cellphonesWWW.");
+        long start = System.currentTimeMillis();
+        try {
+            joinPoint.proceed();
+            long end = System.currentTimeMillis();
+            System.out.println("CLAP CLAP CLAP CLAP CLAP CLAP ");
+            System.out.println("The performance took "+(end-start)+"milliseconds.");
+        } catch (Throwable throwable) {
+            System.out.println("Boo! We want our money back!");
+        }
+    }
+
+    public void takeSeats(){
+        //表演之前
+        System.out.println("The audience is taking their seats");
+    }
+    public void turnOffCellPhones(){
+        System.out.println("The audience is turning off their cellphone");
+    }
+    public void applaud(){
+        //表演之后
+        System.out.println("CLAP CLAP CLAP CLAP CLAP CLAP ");
+    }
+    public void demandRefund(){
+        System.out.println("Boo! we want our money back");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
